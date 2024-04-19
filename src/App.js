e5286e6b522a0189ce0625920eb59e6df566746e
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2024-04-19 11:45:13 -0500

    Aplicacion funcional

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Archivo from './components/Archivo';
+import Prediccion from './components/Prediccion';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Pagina">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Archivo />} />
+          <Route path="/prediccion" element={<Prediccion />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
